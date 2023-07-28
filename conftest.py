126126af718e0e815a126aa6f8cba311281d3d5c
Natalia-Kurylko <nataliahajduk27@gmail.com>
2023-07-28 17:38:45 +0300

    added base_page.py, conftest.py, requirements.txt

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope='function')
+def driver():
+    driver = webdriver.Chrome(ChromeDriverManager(version='114.0.5735.90').install())
+    driver.maximize_window()
+    yield driver
+    driver.quit()
